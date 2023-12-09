51b307fe12742cf417a1fc799c218c1a50d03fe9
Souhrid7 <153090885+Souhrid7@users.noreply.github.com>
2023-12-09 10:59:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
     speak()
 }
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fEycdcml_/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KtvxohGNH/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log('model Loaded!');
@@ -31,3 +31,54 @@ function speak() {
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis)
 }
+function check() {
+    let img=document.getElementById('captured_image')
+    classifier.classify(img,got_result)
+    synth.cancel()
+}
+function got_result(error,results) {
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_emotion_name").innerHTML=results[0].label
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label
+        prediction_1=results[0].label
+        prediction_2=results[1].label
+        speak() 
+        if(results[0].label=="Yo")
+        {
+            document.getElementById("update_emoji").innerHTML="🤟"
+        }
+        if(results[0].label=="Thums-Up")
+        {
+            document.getElementById("update_emoji").innerHTML="👍"
+        }
+        if(results[0].label=="Fist-Bump")
+        {
+            document.getElementById("update_emoji").innerHTML="👊"
+        }
+        if(results[0].label=="Wave")
+        {
+            document.getElementById("update_emoji").innerHTML="🖐️"
+        }
+        if(results[1].label=="Yo")
+        {
+            document.getElementById("update_emoji2").innerHTML="🤟"
+        }
+        if(results[1].label=="Thums-Up")
+        {
+            document.getElementById("update_emoji2").innerHTML="👍"
+        }
+        if(results[1].label=="Fist-Bump")
+        {
+            document.getElementById("update_emoji2").innerHTML="👊"
+        }
+        if(results[1].label=="Wave")
+        {
+            document.getElementById("update_emoji2").innerHTML="🖐️"
+        }
+    }
+    }
+
